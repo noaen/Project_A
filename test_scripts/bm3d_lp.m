0dcbbe4534fff4a0529a035551ab7fabe55d327f
Noa <noaen11@gmail.com>
2017-08-10 16:39:42 +0300

    adding original hgih res low snr image

--- test_scripts/bm3d_lp.m
+++ test_scripts/bm3d_lp.m
@@ -4,7 +4,7 @@
 clc; close all; clear;
 addpath(genpath([ pwd '\..\' ]));
 im = load_untouch_nii('..\mri_images\high_snr_registered.nii');
-sigmaNoise = 70;
+sigmaNoise = 40;
 sigmaBlur = 5;
 [originalIm, hr, lr] = image_prep(im,sigmaNoise, sigmaBlur); %getting a noised version of the original image
 
@@ -32,7 +32,7 @@ end
 % generating top 4 levels with bm3d on level from HR pyramid
 pyr_merged_bm3d = pyr_merged;
 for i = 0:floor((n+1)/2)
-    [PSNR, y_est] = BM3D(originalIm, p_hr{n+1-i}, 70, 'np',0);
+    [PSNR, y_est] = BM3D(originalIm, p_hr{n+1-i}, sigmaNoise, 'np',0);
     pyr_merged_bm3d{n+1-i} = y_est;
     xr = lpr(pyr_merged_bm3d, pfilt);
 %     figure()
@@ -53,7 +53,7 @@ end
 pyr_merged_bm3d_tweaked = pyr_merged;
 % generating top 4 levels with bm3d on level from HR pyramid
 for i = 0:floor((n+1)/2)
-    [PSNR, y_est,~] = BM3DTweaked(originalIm,p_hr{n+1-i},single(p_lr{n+1-i}), 70, 'np',0);
+    [PSNR, y_est,~] = BM3DTweaked(originalIm,p_hr{n+1-i},single(p_lr{n+1-i}), sigmaNoise, 'np',0);
     pyr_merged_bm3d_tweaked{n+1-i} = y_est;
     xr = lpr(pyr_merged_bm3d_tweaked, pfilt);
     figure()

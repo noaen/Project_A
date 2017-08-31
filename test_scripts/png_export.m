7be695b77bd19dbf72536f1971eab97179410059
Noa <noaen11@gmail.com>
2017-08-31 16:45:36 +0300

    adding script to generate png imgs

---
+++ test_scripts/png_export.m
@@ -0,0 +1,26 @@
+%% loading images
+clc; close all; clear;
+addpath(genpath([ pwd '\..\' ]));
+im = load_untouch_nii('..\mri_images\high_snr_registered.nii');
+sigmaNoise = 70;
+sigmaBlur = 5;
+[originalIm, hr, lr] = image_prep(im,sigmaNoise, sigmaBlur); %getting a noised version of the original image
+
+
+%% generating pyramids
+% Laplacian decomposition using 9/7 filters and 5 levels
+pfilt = '9/7';
+n = 7;
+y_lr = lpd(lr, '9/7', n);
+y_hr = lpd(hr,'9/7',n);
+
+imwrite(originalIm,'..\Data\png_images\high_snr_high_res.png', 'png');
+imwrite(lr, '..\Data\png_images\high_snr_low_res.png', 'png');
+imwrite(hr, '..\Data\png_images\low_snr_high_res.png', 'png');
+
+for i = 1:7
+    str_lr = sprintf('lr_lvl_%.0f',i);
+    str_hr = sprintf('hr_lvl_%.0f',i);
+    imwrite(lr, ['..\Data\png_images\' str_lr '.png'], 'png');
+    imwrite(hr, ['..\Data\png_images\' str_hr '.png'], 'png');
+end

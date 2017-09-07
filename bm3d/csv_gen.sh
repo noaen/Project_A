ee2fbc4aa2dd8d97896cbb6eaae807a22f7336fe
Ron and Noa Project <ronnoa@localhost.localdomain>
2017-09-07 17:32:55 +0300

    changed csv generating script and generated csvs.(also changed directories

--- bm3d/csv_gen.sh
+++ bm3d/csv_gen.sh
@@ -4,10 +4,10 @@
 # $3 = image.csv new name
 
 
-./BM3Ddenoising $1 0 ImNoisy.png ImBasic.png ImDenoised.png ImDiff.png ImBias.png ImDiffBias.png 1 bior 0 dct 1 opp high_SNR.png ImResize.png
-mv patch_table.csv output_csvs/$2
-mv image.csv output_csvs/$3
-mv img_for_BM.png output_csvs/$1
+./BM3Ddenoising ../Data/png_images/$1 0 ImNoisy.png ImBasic.png ImDenoised.png ImDiff.png ImBias.png ImDiffBias.png 1 bior 0 dct 1 opp high_SNR.png ImResize.png
+mv patch_table.csv ../Data/csv_files/$2
+mv image.csv ../Data/csv_files/$3
+mv img_for_BM.png ../Data/csv_files/$1
 
 
 

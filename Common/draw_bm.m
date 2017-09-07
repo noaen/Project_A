9dab5b0cfeb1c66c44ea1c288162744321bc2d53
Ronzi <ronziv91@gmail.com>
2017-09-07 10:13:28 +0300

    Added last changes to draw BM function

--- Common/draw_bm.m
+++ Common/draw_bm.m
@@ -1,8 +1,11 @@
-function draw_bm(filename,img, col,row,patch_size)
+function draw_bm(filename,img_csv,img_size,row,col,patch_size)
 % This function receives an image, a file name containing CSV data of block  
 % matching a subscript(x,y) of the picture.
 
-lineInd = sub2ind(size(img),row,col); %shifted row and col for row stack ind
+img = uint8(reshape(img_csv,flip(img_size)));
+figure();
+imshow(img');
+lineInd = sub2ind(size(img),row,col);
 patch_table = get_patchs(filename,lineInd);
 
 M = zeros(length(patch_table),4); %rectangular matrix 
@@ -12,9 +15,24 @@ for i = 1:length(patch_table)
     M(i,2) = pcol;
     M(i,3) = patch_size;
     M(i,4) = patch_size;
+    
 end
+patch_table
+[pcol,prow] = ind2sub(size(img),lineInd); 
 
-img_bm = insertShape(img, 'Rectangle',M);
+% img_re = img';
+
+figure();
+img_bm = insertShape(img', 'Rectangle',M);
+img_bm = insertShape(img_bm, 'Rectangle',[pcol,prow,patch_size,patch_size],'Color','red');
 imshow(img_bm)
+
+figure()
+
+for i = 1:length(patch_table)
+    subplot(2,length(patch_table)/2,i)
+    imshow(imcrop(img',M(i,:)))
+    title(sprintf('# %d',i))
+end
 
 end

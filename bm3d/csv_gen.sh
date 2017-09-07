a6de8b6c1ca9150e05c4db774022a81659cb434a
Ron and Noa Project <ronnoa@localhost.localdomain>
2017-09-07 17:58:24 +0300

    pngs

--- bm3d/csv_gen.sh
+++ bm3d/csv_gen.sh
@@ -4,7 +4,7 @@
 # $3 = image.csv new name
 
 
-./BM3Ddenoising ../Data/png_images/$1 0 ImNoisy.png ImBasic.png ImDenoised.png ImDiff.png ImBias.png ImDiffBias.png 1 bior 0 dct 1 opp high_SNR.png ImResize.png
+./BM3Ddenoising $1 0 ImNoisy.png ImBasic.png ImDenoised.png ImDiff.png ImBias.png ImDiffBias.png 1 bior 0 dct 1 opp high_SNR.png ImResize.png
 mv patch_table.csv ../Data/csv_files/$2
 mv image.csv ../Data/csv_files/$3
 mv img_for_BM.png ../Data/csv_files/$1
